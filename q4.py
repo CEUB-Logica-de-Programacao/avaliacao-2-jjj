ad71c53324179280a6a8d4372c778f550b60fb7d
b1i3ll <111253761+b1i3ll@users.noreply.github.com>
2022-11-07 10:15:37 -0300

    Update q4.py

--- q4.py
+++ q4.py
@@ -25,16 +25,4 @@
 # Dado um numeral romano, converta-o para um número inteiro.
 
 def q4(numeral):
-    def int_to_roman(input):
-    ints = (1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
-    nums = ('M', 'CM', 'D', 'CD','C', 'XC','L','XL','X','IX','V','IV','I')
-    resultado = []
-
-    for i in range(len(ints)):
-        count = int(input / ints[i])
-        result.append(nums[i] * count)
-        input -= ints[i] * count
-    return resultado
-
-if __name__ == '__main__':
-    print(q4('MCMXCIV'))  # 1994
+    pass
