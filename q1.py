2958f528f18937d142a2a40f4aee67b2a31e0435
b1i3ll <111253761+b1i3ll@users.noreply.github.com>
2022-11-07 10:26:34 -0300

    Create q1.py

--- q1.py
+++ q1.py
@@ -20,16 +20,16 @@
 # ```
 
 def q1(names, heights):
- lista=[]
- listacerta=[]
- x = 0
- for i in names
-     lista.append([i, heights[x]])
-     x += 1
- lista=sorted(lista, key=lambda item; item[1]),reverse True
- for i in range(len(lista)):
-     listacerta.append(lista[i][0])
-     return listacerta
+  lista=[]
+  listacerta=[]
+  x = 0
+  for i in names
+      lista.append([i, heights[x]])
+      x += 1
+  lista=sorted(lista, key=lambda item; item[1]),reverse True
+  for i in range(len(lista)):
+      listacerta.append(lista[i][0])
+      return listacerta
 
 
 if __name__ == '__main__':
