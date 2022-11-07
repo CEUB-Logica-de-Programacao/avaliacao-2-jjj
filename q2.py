39954682626579ff1a5f73dfa0804fee2bf1af75
b1i3ll <111253761+b1i3ll@users.noreply.github.com>
2022-11-07 10:12:21 -0300

    Update q2.py

--- q2.py
+++ q2.py
@@ -25,19 +25,19 @@
 
 
 def q2(n):
-def fib(n, dp):
-    if (n <= 1):
-        return 1
-    if(dp[n] != -1 ):
+    def fib(n, dp):
+        if (n <= 1):
+            return 1
+        if(dp[n] != -1 ):
+            return dp[n]
+        dp[n] = fib(n - 1, dp) + fib(n - 2, dp)
         return dp[n]
-    dp[n] = fib(n - 1, dp) + fib(n - 2, dp)
-    return dp[n]
-def countWays(n):
-    dp = [-1 for i in range(n + 1)]
-    fib(n, dp)
-    return dp[n]
-n = 3
-print("maneiras = " + str(countWays(n)))
+    def countWays(n):
+        dp = [-1 for i in range(n + 1)]
+        fib(n, dp)
+        return dp[n]
+    n = 3
+    print("maneiras = " + str(countWays(n)))
 
 
 if __name__ == '__main__':
